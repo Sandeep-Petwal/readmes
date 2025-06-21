2f759e6e596f0bc2fff64d13f224af4492c19c0b
Sandeep-Petwal <sandeeppetwal51@gmail.com>
2025-06-22 02:48:28 +0530

    Deploy website - based on 0df653fefc0079e01bf1b75016edfcf418c16e60

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("D:\\Web\\Sandeep prasad\\Backend + Projects\\Reposities\\readmes\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("D:\\Web\\Sandeep prasad\\Backend + Projects\\Reposities\\readmes\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\Web\\Sandeep prasad\\Backend + Projects\\Reposities\\readmes\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\Web\\Sandeep prasad\\Backend + Projects\\Reposities\\readmes\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\Web\\Sandeep prasad\\Backend + Projects\\Reposities\\readmes\\src\\css\\custom.css"),
-];
